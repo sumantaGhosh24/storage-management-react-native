42089aea115d72d3a8be356e620bf9e993433913
sumanta ghosh <sumantaghosh5333@gmail.com>
2025-01-10 19:17:36 +0530

    setup nativewind

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,21 +1,27 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import { useFonts } from 'expo-font';
-import { Stack } from 'expo-router';
-import * as SplashScreen from 'expo-splash-screen';
-import { StatusBar } from 'expo-status-bar';
-import { useEffect } from 'react';
-import 'react-native-reanimated';
+import "../global.css";
 
-import { useColorScheme } from '@/hooks/useColorScheme';
+import {useEffect} from "react";
+import FontAwesome from "@expo/vector-icons/FontAwesome";
+import {useFonts} from "expo-font";
+import {Stack} from "expo-router";
+import * as SplashScreen from "expo-splash-screen";
+import {StatusBar} from "expo-status-bar";
 
-// Prevent the splash screen from auto-hiding before asset loading is complete.
+import "react-native-reanimated";
+
+export {ErrorBoundary} from "expo-router";
+
 SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
-  const colorScheme = useColorScheme();
-  const [loaded] = useFonts({
-    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
+  const [loaded, error] = useFonts({
+    SpaceMono: require("../assets/fonts/SpaceMono-Regular.ttf"),
+    ...FontAwesome.font,
   });
+
+  useEffect(() => {
+    if (error) throw error;
+  }, [error]);
 
   useEffect(() => {
     if (loaded) {
@@ -28,12 +34,11 @@ export default function RootLayout() {
   }
 
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
+    <>
       <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="+not-found" />
+        <Stack.Screen name="index" options={{headerShown: false}} />
       </Stack>
-      <StatusBar style="auto" />
-    </ThemeProvider>
+      <StatusBar style="auto" backgroundColor="#1D4ED8" />
+    </>
   );
 }
